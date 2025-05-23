286769cbf174f40b96849436066893403f060d6f
Davut <davesizzle@googlemail.com>
2025-05-23 11:04:09 +0200

    first init add color add callback

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,3 +1,5 @@
+// main_screen.dart
+
 import 'package:callback_functions/input_counter_box.dart';
 import 'package:flutter/material.dart';
 
@@ -7,15 +9,35 @@ class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          spacing: 16,
-          children: [
-            InputCounterBox(),
-            InputCounterBox(),
-            InputCounterBox(),
-          ],
+      appBar: AppBar(
+        title: const Text("5.1.6 Callback functions"),
+        backgroundColor: Colors.deepPurple,
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              InputCounterBox(
+                color: Colors.lime,
+                onLengthChanged: () {
+                  debugPrint("Box 1 wurde verändert");
+                },
+              ),
+              InputCounterBox(
+                color: Colors.cyan,
+                onLengthChanged: () {
+                  debugPrint("Box 2 wurde verändert");
+                },
+              ),
+              InputCounterBox(
+                color: Colors.purpleAccent,
+                onLengthChanged: () {
+                  debugPrint("Box 3 wurde verändert");
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
